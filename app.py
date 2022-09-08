9eb42f59f54d3f69dee235b946b770314dda0f12
Patrick Frank <patr.frank@gmx.de>
2022-09-08 10:06:32 +0200

    Endpoint to serve images

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+
+text = [
+	"A dinosaur hunting down an ice cream truck",
+	"A high tech solarpunk utopia in the Amazon rainforest"
+]
+BASE_DIR = "./data/img/"
+img_path = [
+	"dinosaur_ice_cream.png",
+	"rainforest_utopia.png"
+]
 
 app = FastAPI()
 
@@ -6,3 +18,6 @@ app = FastAPI()
 def root() -> str:
 	return "App is up and ready to receive requests."
 
+@app.get("/image", response_class=FileResponse)
+def get_image() -> str:
+	return BASE_DIR + img_path[1]
