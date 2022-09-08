618a35e0935934b4d62e9de6d1b357e89df4f78f
Patrick Frank <patr.frank@gmx.de>
2022-09-08 11:31:15 +0200

    Implement endpoints that serve images

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+from pathlib import Path
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import Response, FileResponse
+from pydantic import BaseModel
+import base64
+
+class Image(BaseModel):
+	id: str
+	base64: str
+
+def base64_encode_image(path: Path) -> str:
+	with open(path, "rb") as image_file:
+		encoded_image_string = base64.b64encode(image_file.read())
+	return encoded_image_string
 
 
 text = [
@@ -12,12 +24,21 @@ img_path = [
 	"rainforest_utopia.png"
 ]
 
+
+
 app = FastAPI()
 
 @app.get("/")
 def root() -> str:
 	return "App is up and ready to receive requests."
 
-@app.get("/image", response_class=FileResponse)
-def get_image() -> str:
-	return BASE_DIR + img_path[1]
+@app.get("/image/file", response_class=FileResponse)
+def get_image_as_file() -> str:
+	return BASE_DIR+img_path[1]
+
+@app.get("/image/json", response_model=Image)
+def get_image_as_json() -> Image:
+	return Image(
+		id=img_path[1],
+		base64=base64_encode_image(Path(BASE_DIR+img_path[1]))
+	)
